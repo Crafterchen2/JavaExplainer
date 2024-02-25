592b38033839627c88880c9ca2f161749d9fcd16
Crafterchen2 <Decker_ben@outlook.de>
2024-02-25 14:06:22 +0100

    Update

--- src/com/deckerben/explainer/Main.java
+++ src/com/deckerben/explainer/Main.java
@@ -1,9 +1,16 @@
 package com.deckerben.explainer;
 
+import com.deckerben.explainer.autos.Segelboot;
+import com.deckerben.explainer.haus.Haus1;
+import com.deckerben.explainer.haus.Haus2;
+
+import static com.deckerben.explainer.Tools.ausgabe;
+import static com.deckerben.explainer.Tools.neueZeile;
+
 public class Main {
 	
 	public static void main(String[] args) {
-	 System.out.println((char) 0);
+		Segelboot sBoot = new Segelboot("1234-ABCD");
 	}
 	
 }
