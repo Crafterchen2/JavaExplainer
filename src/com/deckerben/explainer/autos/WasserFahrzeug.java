d3820d0ce5220941c90195041a6d3b49f064c60f
Crafterchen2 <Decker_ben@outlook.de>
2024-02-25 14:08:14 +0100

    Update

--- src/com/deckerben/explainer/autos/WasserFahrzeug.java
+++ src/com/deckerben/explainer/autos/WasserFahrzeug.java
@@ -2,7 +2,7 @@ package com.deckerben.explainer.autos;
 
 public class WasserFahrzeug extends Fahrzeug{
 	
-	protected WasserFahrzeug(String dasKennzeichen) {
-		super(dasKennzeichen);
+	protected WasserFahrzeug(String dasKennungszeichen) {
+		super(dasKennungszeichen);
 	}
 }
