7e1232b62aeccc01e8aaa713991110aabf4f468a
Crafterchen2 <Decker_ben@outlook.de>
2024-02-25 01:10:14 +0100

    Added Content

---
+++ src/com/deckerben/explainer/Main.java
@@ -0,0 +1,9 @@
+package com.deckerben.explainer;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	 System.out.println((char) 0);
+	}
+	
+}
---
+++ src/com/deckerben/explainer/Tools.java
@@ -0,0 +1,49 @@
+package com.deckerben.explainer;
+
+public class Tools {
+	
+	private Tools(){
+	
+	}
+	
+	public static void neueZeile(){
+		System.out.println();
+	}
+	
+	public static void ausgabe(Object out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(String out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(byte out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(char out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(short out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(int out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(double out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(float out){
+		System.out.println(out);
+	}
+	
+	public static void ausgabe(long out){
+		System.out.println(out);
+	}
+	
+}
